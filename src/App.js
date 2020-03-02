27ae73369609c72c98aa4c6357ea5ab19f7a449e
MediocreIan <idrews094@gmail.com>
2020-03-02 11:44:21 -0600

    wrote html wireframe for resume as outlined in app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
+
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <body>
+          <header>
+            <h1>{props.name} Resume</h1>
+          </header>
+          <section>
+            <h2>Contact Information</h2>
+            <p>Phone Number: 123-456-7890</p>
+            <p>adress: 123 fake street</p>
+            <p>email: fake@fake.com</p>
+            <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg"></img>
+          </section>
+          <section>
+            <h2>education</h2>
+            <h3>school 1</h3>
+            <p>attended never - never</p>
+            <p> studied: didnt</p>
+            <h2>education</h2>
+            <h3>school 1</h3>
+            <p>attended never - never</p>
+            <p> studied: didnt</p>
+            <h2>education</h2>
+            <h3>school 1</h3>
+            <p>attended never - never</p>
+            <p> studied: didnt</p>
+          </section>
+          <section>
+            <h2>employment history</h2>
+            <p>
+              Est incididunt ut commodo veniam exercitation minim reprehenderit ex anim deserunt dolor. Irure deserunt dolore consectetur reprehenderit irure amet sit pariatur ipsum velit ut minim ex. Anim velit in ipsum adipisicing veniam labore. Veniam aliquip cupidatat culpa cillum do sunt consequat ad exercitation cillum laboris tempor cupidatat fugiat. Excepteur pariatur aliquip ad veniam. Qui consequat ipsum dolore sit laborum eu cupidatat eiusmod cupidatat cupidatat exercitation consectetur commodo anim.
+           </p>
+          </section>
+        </body>
       </header>
     </div>
   );
+
 }
 
 export default App;
